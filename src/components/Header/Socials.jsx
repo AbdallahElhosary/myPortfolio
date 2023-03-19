687d057789885c664f0b73d2a828c1036e40ffde
Abdallah Elhosary <114493396+AbdallahElhosary@users.noreply.github.com>
2023-03-20 01:54:45 +0200

    Update Socials.jsx

--- src/components/Header/Socials.jsx
+++ src/components/Header/Socials.jsx
@@ -5,7 +5,7 @@ import "./Socials.css";
 const Socials = () => {
   return (
       <ul className='socials text-center' data-aos="fade-left">
-          <li className="mt-3"><Link to="https://www.facebook.com/profile.php?id=100081034182422" target="_blank"><i className="fa-brands fa-facebook"></i></Link></li>
+          <li className="mt-3"><Link to="https://www.facebook.com/abdallh.elhosary/" target="_blank"><i className="fa-brands fa-facebook"></i></Link></li>
           <li className="mt-3"><Link to="https://www.linkedin.com/in/abdallh-elhosary-a41671220/" target="_blank"><i className="fa-brands fa-linkedin"></i></Link></li>
           <li className="mt-3"><Link to="https://github.com/AbdallahElhosary" target="_blank"><i className="fa-brands fa-github"></i></Link></li>
           <li className="mt-3"><Link to=" https://wa.me/+201113833591" target="_blank"><i className="fa-brands fa-whatsapp"></i></Link></li>
